aa7cacaa18d758654bd142369f5378ba1664d9ae
César Sá <cesar.sa@innowave.tech>
2018-10-03 14:01:47 +0100

    step11

--- src/app/todo-app/todo-app-routing.module.ts
+++ src/app/todo-app/todo-app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { TodoAppComponent } from './todo-app.component';         
+import { TodoAppComponent } from './todo-app.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';       
 
 const routes: Routes = [{                                   
     path: '',                                               
-    component: TodoAppComponent,                              
+    component: TodoAppComponent,
+    children: [                                                           
+      { path: ':index', component: TodoItemComponent },                   
+  ]                              
 }];
 
 @NgModule({
