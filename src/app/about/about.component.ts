7ebf30be3c557a1a4c394a73120bd4eeffc5a35f
César Sá <cesar.sa@innowave.tech>
2018-10-03 11:02:50 +0100

    step7

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';                            
+import { Observable } from 'rxjs';                                      
+
+import { Increment, Decrement } from '@state/counter/counter.actions';  
+import { State } from '@state/counter/counter.state'; 
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  count$: Observable<number>;                                         
+
+  constructor(private store: Store<State>) {                          
+    this.count$ = store.pipe(select('count'));                      
+  }
+
+  increment(){                                                        
+    this.store.dispatch(new Increment());                           
+  }                                                                   
+
+  decrement(){                                                        
+    this.store.dispatch(new Decrement());                           
+  }                                                                   
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';                            
+import { Observable } from 'rxjs';
+
+import { Increment, Decrement } from '@state/counter/counter.actions';  
+import { State } from '@state/counter/counter.state';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  count$: Observable<number>;
+
+  constructor(private store: Store<State>) {                          
+    this.count$ = store.pipe(select('count'));                      
+  }
+
+  increment(){                                                        
+      this.store.dispatch(new Increment());                           
+  }                                                                   
+
+  decrement(){                                                        
+      this.store.dispatch(new Decrement());                           
+  }                                                                   
+
+  ngOnInit() {
+  }
+
+}
